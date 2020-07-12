3477dec5a7f2689995c5df2be96c914920999e66
linda payson <lpayson@iponweb.net>
2020-07-11 22:46:14 -0400

    added searching to user profiles

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,10 +7,11 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profiles', views.UserProfileViewSet)
+router.register('profile', views.UserProfileViewSet)
 
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('',include(router.urls))
 ]
